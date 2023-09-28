e2608a7ed9e3242346d79579845a44d036136bc7
Gaurav Shelke <89787192+gaurav112233@users.noreply.github.com>
2023-09-28 03:10:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId=144553 // constant varaible can't change the value
+let accountEmail="gauravshelke4622@gmail.com"
+var accountPassword="ABCDE@123" 
+accountCity="Pune" 
+
+// accountId=445 // not allowed
+accountEmail="gaurav.shelke20@pccoepune.org"
+accountPassword="VWXYZ@123"
+accountCity="Mumbai" // in this way without mentionig let ,var and const we can declare varioable 
+// in javascript // it is not recommended
+let accountState; // no value is unsigned // so prints undefinec
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) // used to print multiple variables in tabular structure
+
+/*
+  prefer not to use var
+  because of issue in block scope and functional scope
+  Variables that are declared inside a { } block cannot be accessed from outside the block. 
+  Variables declared with the var keyword cannot have block scope and they can be declared inside a { } 
+  block and can be accessed from outside the block.
+ */
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+4) // will give an error // as we are using in nodejs not in browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+let name="Gaurav" // string
+let age=23 // number
+let isLoggeddin=true // boolean
+let state=null
+
+
+// primitive datatypes
+// number -> 2 to power 53
+// bigint
+// string=""// single or double quote
+// boolean true or false
+// null -> standalone value  // some error value is not gotten so null
+// undefined -> abhi value assigned nahi hui hain
+// symbol -> unique
+
+console.log(typeof "Gaurav");
+console.log(typeof undefined) ; // output is undefined
+console.log(typeof null); // null is an object
