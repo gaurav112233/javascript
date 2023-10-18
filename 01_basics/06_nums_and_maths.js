baea2216ebe0a43d1c682d6415f4441021448949
Gaurav Shelke <89787192+gaurav112233@users.noreply.github.com>
2023-10-18 05:31:20 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score=100; // Number datype
+// console.log(score);
+
+// const balance =new Number(100.236); // object 
+// console.log(balance);
+
+// console.log(balance.toString().length); // convert to string
+// console.log(balance.toFixed(2)); // up to two decimal place // round off till 
+
+
+// const othernumber=23.8966;
+// console.log(othernumber.toPrecision(3)); // it gives precision till three digits
+
+// const hundreds=1000000;
+// console.log(hundreds.toLocaleString('en-IN')); // normal indian maths way // 10,00,000
+
+
+
+// ++++++++++++++++++++++   Maths +++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
