4071b051eb4b85166e4952bae74a2485a52bd8f7
Gaurav Shelke <89787192+gaurav112233@users.noreply.github.com>
2023-10-23 02:03:06 +0000

    scopes

--- 03_basics/02_Scopes.js
+++ 03_basics/02_Scopes.js
@@ -2,7 +2,9 @@
 // const b=20
 // var c=30
 
+// var c=300;
 
+let a=300;
 
 
 // Scope is {} curly braces
@@ -11,8 +13,11 @@ if(true)
     let a=10
     const b=20
     var c=30
+    console.log("inner:",a);
 }
 
 // console.log(a); // It throws error due to block scope
 // console.log(b); // It throws error due to block scope
-console.log(c);  // It will print(30)  // Due to var variable has global scope
+// console.log(c);  // It will print(30)  // Due to var variable has global scope
+
+console.log("Outer",a);// let vaiabale has local as well as global scope
