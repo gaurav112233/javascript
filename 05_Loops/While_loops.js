d3ca333ed97ec6b1736351af15212f71dd8c9297
Gaurav Shelke <89787192+gaurav112233@users.noreply.github.com>
2023-10-24 11:27:30 +0000

    whileloop

---
+++ 05_Loops/While_loops.js
@@ -0,0 +1,28 @@
+
+// while loops
+let i=1;
+// while (i<=10) {
+//     console.log(i);
+//     i+=2;
+// }
+
+
+// traverse the array usimg while loop
+let myarray=["thor","Captain america","Jon Snow","Arya Stark"];
+
+// let arr=0;
+// while(arr<myarray.length)
+// {
+//     console.log(myarray[arr]);
+//     arr++;
+// }
+
+
+// Do WHile Loops
+// At least one time execute
+let score=1;
+do {
+   console.log(`Score is : ${score}`);
+   score+=10; 
+} while (score<=100);
+
